f1357432672e665bab2d80a85c700493e5d210bd
Wellington Felix <wfmf@cin.ufpe.br>
2018-04-30 00:54:51 -0300

    Add J48, SVM, Naive Bayes classfiers

--- task2/src/classifier.py
+++ task2/src/classifier.py
@@ -1,13 +1,82 @@
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
 from util import getsoup, preprocess
 from pathlib import Path
+from random import shuffle
+import pandas as pd
 
 
 def main():
-    pos, neg = getpaths()
-    pos = [*map(maphtmltotext, pos)]
-    vectorizer = CountVectorizer()
-    vectorizer.fit_transform(pos)
+    X, y = getdataset()
+    # X, y = shuffledataset(X, y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+    gnb = GaussianNB()  # Naive Bayes
+    gnb.fit(X_train, y_train)
+
+    dt = DecisionTreeClassifier() # Decision Tree
+    dt.fit(X_train, y_train)
+
+    supportvm = svm.SVC() # SVM
+    supportvm.fit(X_train, y_train)
+
+    dtpred = dt.predict(X_test)  # decision tree prediction
+    nbpred = gnb.predict(X_test)  # naive bayes prediction
+    svmpred = supportvm.predict(X_test) # SVM prediction
+
+    print('Decision Tree')
+    print(getstatistics(y_test, dtpred))
+    print('Naive Bayes')
+    print(getstatistics(y_test, nbpred))
+    print('SVM')
+    print(getstatistics(y_test, svmpred))
+
+
+def getstatistics(actual, predicted):
+    size = len(actual)
+    mistakes = 0
+    hits = 0
+    for v1, v2 in [*zip(actual, predicted)]:
+        if v1 == v2:
+            hits += 1
+        else:
+            mistakes += 1
+    return hits/size * 100, mistakes/size * 100
+
+
+def shuffledataset(X, y):
+    dset = [*zip(X, y)] # array of (document, class)
+    shuffle(dset)
+    [_X, _y] = zip(*dset) # unzips dset
+    return list(_X), list(_y)
+
+
+# TODO Use HashingVectorizer instead of TfidfVectorizer
+def getdataset():
+    (pos, neg) = getpaths()
+    positives = [*map(maphtmltotext, pos)]
+    negatives = [*map(maphtmltotext, neg)]
+
+    documents = positives + negatives
+    Y = [1 for _ in range(100)] + [0 for _ in range(100)]
+
+    # savetocsv(documents, Y)
+
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(documents)
+
+    X = vectorizer.transform(documents).toarray()
+
+    return X, Y
+
+
+def savetocsv(docs, y):
+    df = pd.DataFrame(data=docs, columns=["page"])
+    df.to_csv('dataset.csv')
 
 
 def getpaths() -> ([str], [str]):
@@ -20,11 +89,11 @@ def getpaths() -> ([str], [str]):
         bad = Path(str(folder) + "/bad")
 
         for f in good.iterdir():
-            if f.is_file() and f.name.endswith(".html"):
+            if f.is_file() and f.name.endswith("html") or f.name.endswith("htm"):
                 positives.append(str(f))
 
         for f in bad.iterdir():
-            if f.is_file() and f.name.endswith(".html"):
+            if f.is_file() and f.name.endswith("html") or f.name.endswith("htm"):
                 negatives.append(str(f))
     return positives, negatives
 
---
+++ task2/src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='task2',
+    version='',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='wellfelix',
+    author_email='',
+    description=''
+)
