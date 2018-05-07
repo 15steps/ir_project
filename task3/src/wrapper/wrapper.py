6482a76371cca9c99b6195974e4e5dbd6cd9b5bf
Wellington Felix <wfmf@cin.ufpe.br>
2018-05-07 12:36:04 -0300

    Add generic wrapper

--- task3/src/wrapper/wrapper.py
+++ task3/src/wrapper/wrapper.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import requests
 import re
 
@@ -9,27 +9,55 @@ lg = 'http://www.lg.com/us/mobile-phones/g6'  # didnt quite work
 apple = 'https://www.apple.com/iphone-8/specs/'  # working very well
 sony = 'https://www.sonymobile.com/us/products/phones/xperia-xz2/specifications/'
 xiaomi = 'http://www.mi.com/en/mi6/specs/'
+asus = 'https://store.asus.com/us/item/201701AM240000082/A26208-ASUS-ZenFone-3-ZE552KL-5.5'
+moto = 'https://www.motorola.com/us/products/moto-g-gen-6'
+blu = 'http://bluproducts.com/devices/vivo-xl3-plus/'
+htc = 'https://www.htc.com/us/smartphones/htc-u11/#!color=red'
+banggood = 'https://www.banggood.com/Bluboo-S3-6_0-Inch-Sharp-FHD-8500mAh-12V2A-NFC-' \
+           '4GB-RAM-64GB-ROM-MTK6750T-1_5GHz-4G-Smartphone-p-1283089.html?rmmds=category&cur_warehouse=HK'
 
 
 def main():
-    spec = specsection(sony)
-    # print(spec.text)
-    screen = getscreensize(spec.text)
-    camera = getcamerares(spec.text)
-    print(screen)
+    sites = {
+        "samsung": samsung,
+        "lg": lg,
+        "apple": apple,
+        "sony": sony,
+        "xiaomi": xiaomi,
+        "asus": asus,
+        "moto": moto,
+        "blu": blu,
+        "htc": htc,
+        "banggood": banggood
+    }
+
+    regexes = {
+        'screen_size': r'(\d\.\d+(\”|\"))|(\d\.\d+-inch)|(\d\.\d+[ ]Inch)',
+        'camera_res': r'(\d\d|\d\d\.\d+|\d\d[ ])(MP|Megapixels)',
+        'screen_resolution': r'\d{3,4}[ ]?[x*\-by]+[ ]?\d{3,4}',
+        'battery_capacity': r'\d{1,2}[,]?\d{3}[ ]?mah',
+        'ram': r'\s\d[ ]?GB',
+        'internal_memory': r'\d{2,3}[ ]?GB',
+        'processor_speed': r'\d\.\d{1,2}[ ]?(Ghz|GHz)',
+        'weight': r'\d?\.?\d{2,3}[ ]?(oz|g)'
+    }
+
+    for site_name, url in sites.items():
+        print(site_name)
+        specs = specsection(url, verbose=(True if site_name == '' else False))
+
+        for spec_name, rx in regexes.items():
+            print('{0}: {1}'.format(spec_name, getspec(specs, rx)))
+        print('*' * 50)
 
 
-def getscreensize(txt: str) -> [str]:
-    screenrx = re.compile(r'\d[.]\d(\"|-inch)', re.I | re.M)
-    return screenrx.findall(txt)
+def getspec(txt: str, rx):
+    compre = re.compile(rx, re.M)
+    res = compre.search(txt)
+    return None if not res else res.group()
 
 
-def getcamerares(txt: str) -> [str]:
-    mprx = re.compile(r'\d\dMP', re.I | re.M)
-    return mprx.findall(txt)
-
-
-def specsection(url: str):
+def specsection(url: str, verbose=False):
     """
     Attempts to get the spec section
     of a smartphone page
@@ -37,15 +65,23 @@ def specsection(url: str):
     """
     page = requests.get(url)
     bsoup = BeautifulSoup(page.text, 'lxml')
-    rx = r'[\w\W]*(spec)[\w\W]*'
-    specs = bsoup.find_all(class_=re.compile(rx))
-    n = -1
-    for spec in specs:
-        length = len([*spec.children])
-        if length > n:
-            n = length
-            thespec = spec
-    return thespec
+
+    # return bsoup.find('body').getText()
+
+    specs = bsoup.find_all(find_specs)
+    specs = ' '.join([*map(lambda tag: tag.getText(), specs)])
+
+    verbose and print(specs)
+
+    return re.sub(r'[\s]', ' ', specs)
+
+
+def find_specs(tag: Tag):
+    rx = re.compile(r'.*spec.*')
+    for attr, val in tag.attrs.items():
+        if rx.search(str(val)):
+            return True
+    return False
 
 
 if __name__ == '__main__':
